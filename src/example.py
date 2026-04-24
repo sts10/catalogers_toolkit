c1eb3c54057b37c412e0d45a4fbce6a9af9c993e
Sam Schlinkert <sschlinkert@gmail.com>
2026-04-24 17:12:37 -0400

    adds some comments to example code'

--- src/example.py
+++ src/example.py
@@ -4,12 +4,19 @@ from pymarc import MARCReader
 marc_file = "./src/sample-marc-files.mrc"
 
 with open(marc_file, 'rb') as fh:
+    # Parse marc file with pymarc, as usual
     reader = MARCReader(fh)
     for record in reader:
+        # Check that we can read a field
         print(record['245']['a'])
-        this_record = CRecord(record)
-        print(this_record.ocn)
-        print(this_record.field00703_list)
-        print(this_record.field00704_list)
-        print(this_record.field00706_list)
-        print(this_record.field100a)
+        # Now the key move: pass this pymarc-created record object
+        # to catalogers_toolkit's CRecord (clean record) class
+        c_record = CRecord(record)
+        # Now we can call any variable we want, parsed and cleaned 
+        # exactly as defined in catalogers_toolkit's CRecord class
+        # definition 
+        print(c_record.ocn)
+        print(c_record.field00703_list)
+        print(c_record.field00704_list)
+        print(c_record.field00706_list)
+        print(c_record.field100a)
